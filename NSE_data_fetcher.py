0ca139b2a581c11508d0c7e64b86fdab52bf4afa
Pratik Lokhande <pratikthundemon@gmail.com>
2025-02-24 10:53:15 +0530

    Update NSE_data_fetcher.py

--- NSE_data_fetcher.py
+++ NSE_data_fetcher.py
@@ -1,3 +1,5 @@
+
+#%%writefile /content/drive/MyDrive/AI_stock_project/datafetcher.py
 from nsepython import *
 import pandas as pd
 from datetime import datetime, timedelta
@@ -5,28 +7,28 @@ from google.colab import files
 
 class DataFetcher:
     def __init__(self):
-        pass 
-    
+        pass
+
     def get_historical_data(self, symbol: str, days: int = 30, save_csv: bool = False):
-        
+
         end_date = datetime.now().strftime('%d-%m-%Y')
         start_date = (datetime.now() - timedelta(days=days)).strftime('%d-%m-%Y')
-        
+
         data = equity_history(symbol,"EQ", start_date, end_date)
         df = pd.DataFrame(data)
-        
+
 
         if save_csv:
           filename = f"{symbol}_historical_{end_date}.csv"
           df.to_csv(filename, index=False)
           print(f"Data saved as {filename}")
-    
-    
+
+
           files.download(filename)
-        
-          return df
-    
+
+        return df
+
 if __name__ == "__main__":
     fetcher = DataFetcher()
-    df = fetcher.get_historical_data("TRANSRAILL", days=30, save_csv=True)
+    df = fetcher.get_historical_data("REDINGTON", days=30, save_csv=True)
     print(df.head())
